90a7cb973391450edd89995f2f48ef2e0fbd9038
Sreerag ss <you@example.com>
2023-08-21 23:05:52 +0530

    add domain

--- server/mongoDB/connect.js
+++ server/mongoDB/connect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 const connectDB = (url) => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect(
-      "mongodb+srv://sreerag:sreemongo@cluster0.yy9tlkc.mongodb.net/?retryWrites=true&w=majority"
-    )
+    .connect(url)
     .then(() => console.log("DB conected succesfully"))
     .catch((err) => console.log(err));
 };
