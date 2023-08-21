e55610e1f36280b29397d5a039ed49b3438e6722
Sreerag ss <you@example.com>
2023-08-21 22:47:53 +0530

    3rd commit

--- server/mongoDB/connect.js
+++ server/mongoDB/connect.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 const connectDB = (url) => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect(url)
+    .connect(
+      "mongodb+srv://sreerag:sreemongo@cluster0.yy9tlkc.mongodb.net/?retryWrites=true&w=majority"
+    )
     .then(() => console.log("DB conected succesfully"))
-    .catch((err) => alert(err));
+    .catch((err) => console.log(err));
 };
 export default connectDB;
